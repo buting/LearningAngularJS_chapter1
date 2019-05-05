96c9b4d5fc7d26578e1106aae9a92785db3b9bb5
linchunxiao <1917484@qq.com>
2019-05-05 14:24:08 +0800

    1

--- public_html/js/controllers.js
+++ public_html/js/controllers.js
@@ -9,6 +9,9 @@ var helloWorldControllers = angular.module('helloWorldControllers', []);
 helloWorldControllers.controller('MainCtrl', ['$scope', '$location', '$http',
     function MainCtrl($scope, $location, $http) {
         $scope.message = "Hello World--MainCtrl";
+		$scope.addbtn_clicked = function(){
+		  alert("abc")	
+		};
     }]);
 
 helloWorldControllers.controller('ShowCtrl', ['$scope', '$location', '$http',
